4f7290dc223672c8690f7a3988b84bced5ba7cb1
ali <alimartondi88@gmail.com>
2022-05-17 10:35:09 +0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import FormEveryday from "./components/FormEveryday";
+import StudentForm from "./components/StudentForm";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <img
+          src="images/goodfood-header-pattern.svg"
+          alt="goodfood-header-pattern"
+          className="img-fluid"
+        />
       </header>
+      <main>
+        <StudentForm />
+      </main>
+      <footer>Yeah I'm Footer</footer>
     </div>
   );
 }
---
+++ src/components/FormEveryday.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function FormEveryday() {
+  return (
+    <section>
+      <div className="container-xxl">
+        <div className="col-lg-8">
+          <h2 className="text-uppercase">Student Details</h2>
+        </div>
+      </div>
+    </section>
+  );
+}
